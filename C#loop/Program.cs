0fceb3bc7023f797640a744a21fe59f23a359c96
GodzillaViper <kamran.allahverdi@elev.ga.ntig.se>
2024-12-17 13:14:33 +0100

    first commit

---
+++ C#loop/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //uppgift 1
+        //Console.WriteLine("Udda tall mellan 1-99");
+
+        //for (int n = 1; n <(99+1); n++){
+            //if(n % 2 != 0)
+            //{
+                //Console.WriteLine(n);
+            //}
+        //}
+
+
+        //uppgift 2
+        //for (int i = 10; i > 0; i--){
+            //for (int j = 0; j < i; j++){
+                //Console.Write("#"); 
+            //}
+            //Console.WriteLine("");
+        //}
+
+        //uppgift 3 
+        //int tries = 0;
+        //string password = "password";
+
+        //while (true) {
+            //Console.Write("Your guess: "); 
+            //string? guess = Console.ReadLine();
+            //tries++;
+
+            //if (guess == password) {
+                //Console.WriteLine("You guessed the password correctly !");
+                //Console.WriteLine($"Tries: {tries}");
+                //break;
+            //}
+
+            //Console.WriteLine("Wrong, try again.");
+        //}
+        
+        //uppgift4 
+        //Console.Write("Enter balance: ");
+        //double balance = double.Parse(Console.ReadLine());
+
+        //Console.Write("Enter goal: ");
+        //double goal = double.Parse(Console.ReadLine());
+
+        //Console.Write("Enter interest: ");
+        //double interest = double.Parse(Console.ReadLine());
+
+        //double rate = interest / 100; 
+        //int years = 0;
+
+        //while (balance < goal)
+        //{
+            //balance += balance * rate;
+            //years++;
+        //}
+
+        //Console.WriteLine($"It will take {years} years to reach your goal of {goal:C}.");
+
+
+    }
+}
